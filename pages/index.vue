dcc12e51d2f1b42712d60a106f3231a4fc19efe4
Larry W. <l422y@l422y.com>
2024-02-28 22:58:58 -0500

    feat(product store): add initialization and product addition functionalities

--- pages/index.vue
+++ pages/index.vue
@@ -1,38 +1,47 @@
 <template>
-  <div>
-    <h1>Products</h1>
-    <div class="products--list">
-      <ProductRow v-for="product in products" :product="product" />
-    </div>
-    <footer>
-      <button @click="loadProducts">Load Products</button>
-      <button @click="addProduct">Add New Product</button>
-    </footer>
+    <div>
+        <h1>Products</h1>
+        <div class="products--list">
+            <ProductRow v-for="product in products" :product="product"/>
+        </div>
+        <footer>
+            <button @click="productsStore.initialize">Initialize Store</button>
+            <button @click="addProduct">Add New Product</button>
+        </footer>
 
-    <h3>Product Store Current Value</h3>
-    <pre>{{ useProductsStore().products }}</pre>
-  </div>
+        <h3>Product Store Current Value</h3>
+        <pre>{{ useProductsStore().products }}</pre>
+
+        <h3>
+            Product One
+            <pre>{{ prductOne ? prductOne : 'Not Loaded' }}</pre>
+        </h3>
+    </div>
 </template>
 <script lang="ts" setup>
-const productsStore = useProductsStore();
+const productsStore = useProductsStore()
 
-const products = computed(() => useProductsStore().products);
+const products = computed(() => Array.from(useProductsStore().products.values()))
 
-const loadProducts = () => {
-  productsStore.loadProducts();
-};
+const prductOne = computed(() => useProductsStore().products.get("product-1"))
 
 const addProduct = () => {
-  productsStore.addProduct();
-};
+    const productNumber = useProductsStore().products.size + 1
+    productsStore.addProduct({
+        id: `product-${productNumber}`,
+        name: `Product ${productNumber}`,
+        price: 100,
+    })
+}
 </script>
 <style>
 .products--list {
-  display: grid;
-  align-content: center;
-  gap: 0.5rem;
+    display: grid;
+    align-content: center;
+    gap: 0.5rem;
 }
+
 footer {
-  margin-top: 1rem;
+    margin-top: 1rem;
 }
 </style>
--- stores/products.ts
+++ stores/products.ts
@@ -1,21 +1,32 @@
-export const useProductsStore = defineStore('products', () => {
-  const productsMap = reactive(new Map<string, any>());
-  function initialize() {
-    setTimeout(() => {
-      productsMap.set('product-1', {
-        id: 1,
-        name: 'product one',
-        type: 'Clothing',
-      });
-      productsMap.set('product-2', {
-        id: 2,
-        name: 'product two',
-        type: 'Shoes',
-      });
-    }, 5000);
-  }
+export const useProductsStore = defineStore("products", () => {
+    const productsMap = reactive(new Map<string, any>())
 
-  return {
-    productsMap,
-  };
-});
+    function initialize() {
+        setTimeout(() => {
+            productsMap.set("product-1", {
+                id: 1,
+                name: "product one",
+                type: "Clothing",
+            })
+            productsMap.set("product-2", {
+                id: 2,
+                name: "product two",
+                type: "Shoes",
+            })
+        }, 5000)
+    }
+
+    function addProduct({id, name, type}: {id: string, name: string, type: string}) {
+        productsMap.set(name, {
+            id,
+            name,
+            type
+        })
+    }
+
+    return {
+        initialize,
+        products: productsMap,
+        addProduct
+    }
+})
